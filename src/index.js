51bbef196436b6f375726d1bdbfbe1eedcfa0a7b
IrynaAn <airyna1@mail.ru>
2022-02-09 17:17:48 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   
+    let decoded = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        let l = expr.substr(i, 10);
+        decoded = decoded+ decodeLetter(l); 
+    }
+    function decodeLetter(l) {
+        if (l === '**********') return ' ';
+         let mCode = '';
+        for (let i = 0; i < l.length; i += 2) {
+            let code = l.substr(i, 2);
+            if (code =='10') {mCode =mCode+ '.'};
+            if (code =='11') {mCode =mCode+ '-'};
+           
+        }
+        return MORSE_TABLE[mCode];
+    }
+    return decoded;
+
 }
 
 module.exports = {
